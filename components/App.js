3d4494084195018734ea64c6588abbd206e4086f
Len Smith <len@barrison.com>
2017-01-04 19:28:06 -0800

    Make App.js work for both files

---
+++ components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles/AppStyles'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
---
+++ index.android.js
@@ -0,0 +1,4 @@
+import App from './components/App'
+import {AppRegistry} from 'react-native'
+
+AppRegistry.registerComponent('ReactEverywhereNativeDemo', () => App);
